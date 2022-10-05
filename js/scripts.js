61524c24ac740bc272a0e9c5eb6b9027f7ab8367
B00M808 <chivonnelondoncr8tive@gmail.com>
2022-10-04 20:30:31 -0700

    Alt image, Created eventListener and formatted dob

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ fetch("https://randomuser.me/api/?results=12&nat=us")
 
 //Get & Display 12 Random Users
 function generateImage(result) {
-    console.log(result)
+    //console.log(result)
     let card;
     
     result.forEach((result, index) => {
@@ -34,38 +34,41 @@ function generateImage(result) {
     })
 }
 
-function displayModal(card, index){
-    console.log(card, index);
-    
+function displayModal(card, index) {
+    //console.log(card, index);
 
     let modalContainer = document.createElement("div");
     modalContainer.className = 'modal-container';
 
     gallery.appendChild(modalContainer);
    
-//Create A Modal Window
+    //Stored the card.dob.date in a new variables. Worked with the split() method to format the date of birth  
+    let output = card?.dob?.date.split("T")[0].split("-");
+    let year = output[0];
+    let month = output[1];
+    let day = output[2]
 
-modalContainer.innerHTML = `
+    //Created A Modal Window
+    modalContainer.innerHTML = `
                 <div class="modal">
                     <button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>
                     <div class="modal-info-container">
-                        <img class="modal-img" src="${card.picture.thumbnail}" alt="${card.name.first}">
-                        <h3 id="name" class="modal-name cap">${card.name.first} ${card.name.last}</h3>
-                        <p class="modal-text">${card.email}</p>
-                        <p class="modal-text cap">${card.location.city}</p>
+                        <img class="modal-img" src="${card?.picture?.medium}" alt="${card?.name?.first}">
+                        <h3 id="name" class="modal-name cap">${card?.name?.first} ${card?.name?.last}</h3>
+                        <p class="modal-text">${card?.email}</p>
+                        <p class="modal-text cap">${card?.location?.city}</p>
                         <hr>
-                        <p class="modal-text">${card.phone}</p>
-                        <p class="modal-text">${card.location.street.number}, ${card.location.street.name}, ${card.location.city}, ${card.location.state} ${card.location.postcode}</p>
-                        <p class="modal-text">Birthday: ${card.dob.date}</p>
+                        <p class="modal-text">${card?.phone}</p>
+                        <p class="modal-text">${card?.location?.street?.number}, ${card?.location?.street?.name}, ${card?.location?.city}, ${card?.location?.state} ${card?.location?.postcode}</p>
+                        <p class="modal-text">Birthday: ${month}/${day}/${year}</p>
                     </div>
                 </div
-                
-                
-`
+    `
+/* Created an eventListener to listen for clicks specfically engaging the X button and not allowing clicks elsewhere on the screen. Checked if the e.target's tagName was equal to "STRONG" then gallery.removeChild(gallery.lastElementChild) because the modal is appended as the last element of the gallery div. */
 
-//Event Listener
-
-
-
-
+    modalContainer.addEventListener("click", (e) => {
+        if(e.target.tagName === 'STRONG'){
+            gallery.removeChild(gallery.lastElementChild);
+        }
+    })
 }
